7b53bc418f4b7c3ada325039503db4e16a7dde8a
ThatsMyMute <75107278+ThatsMyMute@users.noreply.github.com>
2021-05-26 00:44:34 -0400

    Update bubble_gum.lua
    Added anti afk

--- bubble_gum.lua
+++ bubble_gum.lua
@@ -5,9 +5,20 @@
     local string_1 = "BlowBubble";
     local Target = game:GetService("ReplicatedStorage").NetworkRemoteEvent;
     Target:FireServer(string_1);
-    local sell = "SellBubble";
-    local sell1 = "Sell";
-    local Target = game:GetService("ReplicatedStorage").NetworkRemoteEvent;
-    Target:FireServer(sell, sell1);
+local sb = "SellBubble";
+local tw = "TwilightSell";
+local Target = game:GetService("ReplicatedStorage").NetworkRemoteEvent;
+Target:FireServer(sb, tw);
 end
 end
+
+getgenv().antiafk = true; --change to true to work change to false to turn off
+    if getgenv().antiafk == true then
+        local vu = game:GetService("VirtualUser")
+game:GetService("Players").LocalPlayer.Idled:connect(function()
+   vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+   wait(1)
+   vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+end)
+end
+-----------------  Updated for adding anti afk %90 of executors supported.
